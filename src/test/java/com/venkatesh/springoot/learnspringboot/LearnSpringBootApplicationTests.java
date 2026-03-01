265b2ebaf3ea8d6b2eff8b4f9aa1592258619304
Venkatesh <venkateshwaran.v2002@gmail.com>
2026-03-01 20:29:05 +0530

    Initial Commit

---
+++ src/test/java/com/venkatesh/springoot/learnspringboot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venkatesh.springoot.learnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
